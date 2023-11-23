924e1669a88ec464f96d061b62a3fb343a47af62
hunterxcobby <solomonsefah13@gmail.com>
2023-11-23 16:28:24 +0000

    arithmetric operators

---
+++ shell_scripting/0x04-loops_conditions_and_parsing/operators/practice/arithmetic_operators.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+let "result = 5 + 3"
+echo "$result"
+
+let "result = 5 - 3"
+echo "$result"
+
+let "result = 5 * 3"
+echo "$result"
+
+let "result = 5 / 3"
+echo "$result"
+
+let "result = 5 % 10"
+echo "$result"
