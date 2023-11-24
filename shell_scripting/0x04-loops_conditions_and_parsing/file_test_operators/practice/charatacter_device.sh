b4843691e3c3ac419feb27c6c5e33b41a46ac6c1
hunterxcobby <solomonsefah13@gmail.com>
2023-11-24 10:46:03 +0000

    check if a file is a character file

---
+++ shell_scripting/0x04-loops_conditions_and_parsing/file_test_operators/practice/charatacter_device.sh
@@ -0,0 +1,17 @@
+device0="/dev/sda2"    # /   (root directory)
+if [ -b "$device0" ]
+then
+  echo "$device0 is a block device."
+fi
+
+# /dev/sda2 is a block device.
+
+
+
+device1="/dev/ttyS1"   # PCMCIA modem card.
+if [ -c "$device1" ]
+then
+  echo "$device1 is a character device."
+fi
+
+# /dev/ttyS1 is a character device.
