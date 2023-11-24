adeb92dfd817ba6e3859020fca6fb2790151cc07
hunterxcobby <solomonsefah13@gmail.com>
2023-11-24 10:46:26 +0000

    to demonstrate if a file is a pipe file

---
+++ shell_scripting/0x04-loops_conditions_and_parsing/file_test_operators/practice/pipe.sh
@@ -0,0 +1,9 @@
+function show_input_type()
+{
+   [ -p /dev/fd/0 ] && echo PIPE || echo STDIN
+}
+
+show_input_type "Input"                           # STDIN
+echo "Input" | show_input_type                    # PIPE
+
+# This example courtesy of Carl Anderson.
