1a3525fc27c654a4e03a7b860e585bae2dae30bf
hunterxcobby <solomonsefah13@gmail.com>
2024-01-19 22:09:51 +0000

    modify site to change port

--- shell_scripting/0x0A-configuration_management/practice/site.pp
+++ shell_scripting/0x0A-configuration_management/practice/site.pp
@@ -1,18 +1,48 @@
+# Define parameters
+$web_content = 'Hello, Puppet Web Server!'
+$web_port    = 8080  # Change to your desired port, e.g., 8080
 
+# Install Apache
 package { 'apache2':
-    ensure => installed,
+  ensure => installed,
 }
 
+# Configure Apache service
 service { 'apache2':
   ensure  => running,
   enable  => true,
 }
 
-file { '/var/www/html/':
-  ensure => directory,
-}
-
+# Create web page
 file { '/var/www/html/index.html':
   ensure  => file,
-  content => 'Hello, Puppet Web Server!',
+  content => $web_content,
+}
+
+# Update default Apache port and bind to localhost
+file { '/etc/apache2/ports.conf':
+  ensure  => present,
+  content => "Listen $web_port\n",
+  notify  => Service['apache2'],
+}
+
+# Configure VirtualHost for localhost
+file { '/etc/apache2/sites-available/localhost.conf':
+  ensure  => present,
+  content => "
+    <VirtualHost *:$web_port>
+        DocumentRoot /var/www/html
+        ServerName localhost
+        ServerAlias 127.0.0.1
+    </VirtualHost>
+  ",
+  notify  => Service['apache2'],
+  require => File['/etc/apache2/ports.conf'],
+}
+
+# Enable the VirtualHost
+file { '/etc/apache2/sites-enabled/localhost.conf':
+  ensure  => link,
+  target  => '/etc/apache2/sites-available/localhost.conf',
+  require => File['/etc/apache2/sites-available/localhost.conf'],
 }
