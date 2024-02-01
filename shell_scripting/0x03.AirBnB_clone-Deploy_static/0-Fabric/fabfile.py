8345bc1de72f844e2006cbf24dbafc58326cb391
hunterxcobby <solomonsefah13@gmail.com>
2024-02-01 14:14:14 +0000

    make a dir with the fabfile

--- shell_scripting/0x03.AirBnB_clone-Deploy_static/0-Fabric/fabfile.py
+++ shell_scripting/0x03.AirBnB_clone-Deploy_static/0-Fabric/fabfile.py
@@ -1,6 +1,13 @@
+#!/usr/bin/python3
+
 from fabric import task
 
 @task
 def hello(ctx, who="world"):
     print(f"Hello, {who}!")
 
+@task
+def folder(ctx):
+    result = ctx.run("mkdir folder/")
+    if result.failed:
+        print("Failed to create folder")
