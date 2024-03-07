b1570b19b7c63a41659e66d088ac47acb50cc0e6
hunterxcobby <solomonsefah13@gmail.com>
2024-03-07 15:21:20 +0000

    check status code

--- shell_scripting/0x15-api/3-tasks/0-gather_data_from_an_API.py
+++ shell_scripting/0x15-api/3-tasks/0-gather_data_from_an_API.py
@@ -2,8 +2,6 @@
 import requests
 import sys
 
-
-
 def check_arg(args):
     if len(args) != 2:
         print("Please made sure to id and nothing else")
@@ -14,6 +12,9 @@ def check_arg(args):
 id = check_arg(args = sys.argv)
 
 def getEmployersDetails(id):
+    
+    url_path = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    print(url_path.status_code)
     print(id)
 
 getEmployersDetails(id)
